05a58544b0dd412d25b143894851b436e54dd691
marc <marc@cockroachlabs.com>
2017-06-08 15:02:27 -0400

    tweak

--- pkg/cmd/request-cert/main.go
+++ pkg/cmd/request-cert/main.go
@@ -1,7 +1,11 @@
 package main
 
+// Inspired by: https://github.com/kelseyhightower/certificate-init-container/blob/master/main.go
+
 import (
+	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -9,7 +13,21 @@ import (
 	"k8s.io/client-go/rest"
 )
 
+var (
+	addresses = flag.String("addresses", "", "comma-separated list of DNS names and IP addresses")
+	certsDir  = flag.String("certs-dir", os.ExpandEnv("${HOME}/.cockroach-certs"), "certs directory")
+)
+
 func main() {
+	flag.StringVar(&clusterDomain, "cluster-domain", "cluster.local", "Kubernetes cluster domain")
+	flag.StringVar(&hostname, "hostname", "", "hostname as defined by pod.spec.hostname")
+	flag.StringVar(&namespace, "namespace", "default", "namespace as defined by pod.metadata.namespace")
+	flag.StringVar(&podName, "pod-name", "", "name as defined by pod.metadata.name")
+	flag.StringVar(&podIP, "pod-ip", "", "IP address as defined by pod.status.podIP")
+	flag.StringVar(&serviceNames, "service-names", "", "service names that resolve to this Pod; comma separated")
+	flag.StringVar(&serviceIPs, "service-ips", "", "service IP addresses that resolve to this Pod; comma separated")
+	flag.StringVar(&subdomain, "subdomain", "", "subdomain as defined by pod.spec.subdomain")
+	flag.Parse()
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
